0f7ca8b67f739673a88585b980447b9647ea218c
wrahimee1999 <wrahimee1999@gmail.com>
2025-03-15 14:29:35 +0430

    favorites section completed

--- src/Pages/Favorites.tsx
+++ src/Pages/Favorites.tsx
@@ -1,8 +1,24 @@
+import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../contexts/MovieContext";
 import "../css/Favorites.css";
+import { Movie } from "../components/MovieCard";
+
 export default function Favorites() {
-    return (
-        <div className="favorites-empty">
-            <h2>No Fovorites Movie Yet!</h2>
-        </div>
-    );
+  const { favorites } = useMovieContext();
+  const movies: Movie[] = favorites ?? []; // Ensures movies is always an array
+
+  return movies.length > 0 ? (
+    <div className="favorites">
+      <h3>Your Favorites</h3>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet!</h2>
+    </div>
+  );
 }
